2e7f517c78f2214a006b5ca7d641021fe1e98ee4
vickichowder <vechow12@gmail.com>
2016-05-02 14:57:12 -0400

    Prepare to integrate with AWS API Gateway

--- src/index.js
+++ src/index.js
@@ -1,53 +1,58 @@
-require('dotenv').load();
+var dotenv = require('dotenv');
+dotenv.load();
 
-var http       = require('http'),
-    AlexaSkill = require('./AlexaSkill'),
-    APP_ID     = process.env.APP_ID,
-    SC = require('soundcloud'),
-    SC_USER_ID    = process.env.SOUNDCLOUD_USER,
-    SC_CLIENT_ID  = process.env.SOUNDCLOUD_CLIENT;
+var http = require('http');
+var request = require('request');
+var util = require('util');
+var SC = require('soundcloud');
+console.log(1);
+var AlexaSkill = require('./AlexaSkill');
+console.log(2);
+var APP_ID = process.env.APP_ID;
+console.log(3);
+
+console.log(4);
+var SC_USER_ID = process.env.SOUNDCLOUD_USER;
+var SC_CLIENT_ID = process.env.SOUNDCLOUD_CLIENT;
+var SC_USERNAME = process.env.SOUNDCLOUD_USERNAME;
+var resolve_url = 'http://api.soundcloud.com/resolve?';
 
 var PLAYLIST_MAP = {
-    OdeszaNoSleep: "206787028",
-    // GalimatiasVoyage: "2619526011",
-    Songs: "149439366"
-    // ChetFaker: "15690151",
-    // Flume: "165797011",
-    // TaKu: "11055981",
-    // Kai: "1000"
+    odesza: "odesza-no-sleep",
+    galimatias: "2619526011",
+    songs: "songs"
+    ChetFaker: "15690151",
+    Flume: "165797011",
+    TaKu: "11055981"
 };
 
 SC.initialize({
-    client_id: SC_CLIENT_ID,
+    client_id: SC_CLIENT_ID
 });
 
 var SoundCloudPlay = function(){
-  AlexaSkill.call(this, APP_ID);
+  AlexaSkill.call(this, APP_ID)
 };
-
-var player;
 
 SoundCloudPlay.prototype = Object.create(AlexaSkill.prototype);
 SoundCloudPlay.prototype.constructor = SoundCloudPlay;
-
 SoundCloudPlay.prototype.eventHandlers.onSessionStarted = function(sessionStartedRequest, session){
-  // What happens when the session starts? Optional
   console.log("onSessionStarted requestId: " + sessionStartedRequest.requestId
-      + ", sessionId: " + session.sessionId);
+      + ", sessionId: " + session.sessionId)
 };
 
 SoundCloudPlay.prototype.eventHandlers.onLaunch = function(launchRequest, session, response){
   var output = 'Hello from Sound Cloud. ' +
-    'What playlist do you want to hear?';
+    'What playlist do you want to hear?'
 
   var reprompt = 'Which artist do you want to hear?';
-  response.ask(output, reprompt);
+  response.ask(output, reprompt)
 
   console.log("onLaunch requestId: " + launchRequest.requestId
-      + ", sessionId: " + session.sessionId);
+      + ", sessionId: " + session.sessionId)
 };
 
-// var handleListPlaylistRequest = function(intent, session, response){
+// function handleListRequests(intent, session, response){
 //   var playlistNames = [];
 //   data.forEach(function(playlist){
 //     var playlistName = playlist.permalink.replace(/\.\s*/g, '').replace(/\-\s*/g, '');
@@ -56,48 +61,58 @@ SoundCloudPlay.prototype.eventHandlers.onLaunch = function(launchRequest, sessio
 //   return playlistNames;
 // };
 
-var handlePlayPlaylistRequest = function(intent, session, response){
-  var playlistId = PLAYLIST_MAP[intent.slots.playlist.value];
-  player = SC.stream('playlist/' + playlistId);
-  speechText = "Playing " + intent.slots.playlist.value + " from Sound Cloud.";
+function handlePlayRequest(intent, session, response){
+  var playlist = intent.slots.Playlist.value.replace(/\.\s*/g, '')
+  var playlistName = PLAYLIST_MAP[playlist]
+
+  var speechText = "Playing " + playlist + " from Soundcloud"
+
   speechOutput = {
       speech: speechText,
       type: AlexaSkill.speechOutputType.PLAIN_TEXT
   };
   response.tell(speechOutput);
-  player.then(function(player){
-    player.play();
-  });
+
+  $scope.streamTrack = function( track_url ){
+       var formatted_url = 'http://soundcloud.com' + track_url;
+
+       SC.get( '/resolve', { url: formatted_url }, function( track ){
+           SC.stream( '/tracks/' + track.id, function( sm_object ){
+               $scope.tracks.push( sm_object );
+               console.log( sm_object.url );
+              //  return( sm_object.url );
+           });
+       });
+   }
 };
 
-var handlePauseRequest = function(intent, session, response){
-  player.then(function(player){
-    player.pause();
-  });
+
+function handlePauseRequest(intent, session, response){
 };
 
 SoundCloudPlay.prototype.intentHandlers = {
   // WIP
-  // ListPlaylistIntent: function(intent, session, response){
+  // ListIntent: function(intent, session, response){
   //   var listPlaylists = handleListPlaylistRequest(intent, session, response);
   //   response.tell(listPlaylists);
   // },
 
-  PlayPlaylistIntent: function(intent, session, response){
-    handlePlayPlaylistRequest(intent, session, response);
+  "PlayIntent": function(intent, session, response){
+    handlePlayRequest(intent, session, response)
   },
 
-  PauseIntent: function(intent, session, response){
-    handlePauseRequest(intent, session, response);
+  "PauseIntent": function(intent, session, response){
+    handlePauseRequest(intent, session, response)
   },
 
-  HelpIntent: function(intent, session, response){
-    var speechOutput = 'What playlist do you want to hear?';
-    response.ask(speechOutput);
+  "HelpIntent": function(intent, session, response){
+    var speechOutput = 'What playlist do you want to hear?'
+    response.ask(speechOutput)
   }
 };
 
 exports.handler = function(event, context) {
-    var skill = new SoundCloudPlay();
-    skill.execute(event, context);
+    var soundCloudPlay = new SoundCloudPlay()
+    soundCloudPlay.execute(event, context)
 };
+console.log('last line');
